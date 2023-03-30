081becb429fd55903a98f7d1972083319638a72a
Daniel Corey <dcorey2814@gmail.com>
2023-03-30 15:19:06 +0200

    added folder for (3,12)

---
+++ matroids_d3n12/isolate3Lines_d3n12.jl
@@ -0,0 +1,38 @@
+using Combinatorics
+using Oscar
+
+include("src/isolate3Lines.jl")
+
+db = Polymake.Polydb.get_db();
+collection = db["Matroids.Small"];
+cursor=Polymake.Polydb.find(collection, Dict("RANK" => 3,"SIMPLE"=>true,"N_ELEMENTS"=>12), opts=Dict("limit"=>10000));
+
+n3C12 = collect(powerset(1:12, 3, 3));
+n3C12 = sort(n3C12, by =  x-> reverse(x));
+
+io = open("matroids_d3n12/d3n12_3lines-100K.dat", "w")
+
+#i = 0;
+for c in cursor
+    M = Matroid(c)
+    ns = count_3_lines_thru_all_points(M)
+    
+    if length(ns) == 0
+        continue
+    end
+
+    if minimum(ns) >= 3
+        write(io, to_revlex(M, n3C12) * "\n")
+    end
+
+    # checkpoint
+#    if (i%100000 == 0)
+#        close(io)
+#        global io = open("matroids_d3n12/d3n12_3lines-100K.dat", "a")
+#    end
+
+#    global i += 1
+#    println(i)
+end
+
+close(io)
---
+++ matroids_d3n12/test.jl
@@ -0,0 +1,7 @@
+i = 0
+
+for j in 1:10
+    println(i%2)
+    global i+=1
+    #global i = k
+end
