2fb2986ef3d2f664e8b9ef5e6b26420b8ce3bbea
Daniel Corey <corey@milnor.math.tu-berlin.de>
2023-04-11 14:11:39 +0200

    modify test univariate

--- d3n12/test_smooth_univariate.jl
+++ d3n12/test_smooth_univariate.jl
@@ -1,3 +1,9 @@
+# ARGS[1] = input_file_path
+# ARGS[2] = output_file_label
+
+
+
+
 using Combinatorics
 using Oscar
 
@@ -7,8 +13,8 @@ include(joinpath(currentDir, "src/matroid_realization.jl"));
 include(joinpath(currentDir, "src/reduceIdealsv2.jl"));
 include(joinpath(currentDir, "src/JacobianCriterion.jl"));
 
-
-d3n12_pu = vec(readlines(joinpath(currentDir, "d3n12/sorting_7/principal_univariate.dat")));
+d3n12_pu = vec(readlines(joinpath(currentDir, ARGS[1])));
+#d3n12_pu = vec(readlines(joinpath(currentDir, "d3n12/sorting_7/principal_univariate.dat")));
 
 
 #checks for integer generator
@@ -23,17 +29,20 @@ end
 
 singularDir = make_directory(joinpath(currentDir, "d3n12/singular"))
 
-file_pm_singular = open(joinpath(singularDir, "principal_univariate_singular.dat"), "w")
-file_pm_smooth = open(joinpath(singularDir, "principal_univariate_smooth.dat"), "w")
+
+file_pm_singular = open(joinpath(singularDir, string("principal_univariate_singular.",ARGS[2],".dat")), "a")
+file_pm_smooth = open(joinpath(singularDir, string("principal_univariate_smooth.",ARGS[2],".dat")), "a")
 
 
-for z in 1:length(d3n12_pu)
+#for z in 1:length(d3n12_pu)
+for z in 4331:length(d3n12_pu)
+
 
     Mzstr = d3n12_pu[z]
-    Mz = matroid_from_revlex_basis_encoding(Mzstr, 3, 12)
-    Igens, Sgens = matroid_to_reduced_expression(Mz, QQ)
+    Mz = matroid_from_revlex_basis_encoding(Mzstr, 3, 12);
+    Igens, Sgens = matroid_to_reduced_expression(Mz, QQ);
 
-    I = simplified_2_singular_locus(Igens, Sgens)
+    I = simplified_2_singular_locus(Igens, Sgens);
     
     if !isone(I)
         write(file_pm_singular, String(Mstr), "\n")
