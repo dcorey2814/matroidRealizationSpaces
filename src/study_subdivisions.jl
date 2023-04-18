db375edf4272d9cf3ba465df6be69b67ab8571e3
dante <danteluber@gmail.com>
2023-04-18 07:07:42 +0200

    code to look at subdivisions

---
+++ src/study_subdivisions.jl
@@ -0,0 +1,70 @@
+#map vertex of hypersimplex to set
+function vertex_to_set(x)
+    set = []
+    for t in 1:length(x)
+        if x[t] == 1
+            push!(set,t)
+        end
+    end
+    return set
+end
+
+#compute corank vector of a matroid
+function corank_vector(M)
+    d = rank(M)
+    n = length(matroid_groundset(M))
+    
+    B = bases(M)
+    
+    D = pm.polytope.hypersimplex(d,n)
+    V = D.VERTICES
+    
+    x = zeros(length(V[:,1]))
+
+    
+    for t in 1:length(V[:,1])
+        
+        b = vertex_to_set(VD[t,:])
+        
+        x[t] = d - rank(M,b)#corank
+        #x[t] = corank(M,b)
+        
+    end
+    return x
+end
+
+#checks if polytope is generalized permutahedron. input polytope needs to be convex_hull(point configuration)
+
+function is_genperm(P)
+    
+    E = [vertices(e) for e in faces(P,1)]
+    
+    yes = []
+    
+    for edge in E
+        
+        diff = edge[1]-edge[2]
+        
+        supp = [i for i in 1:length(diff) if !(diff[i] == 0)]
+        
+        if !(length(supp) == 2)
+            
+            return 1 == 2
+            
+            
+        else
+            
+             if diff[supp[1]] == -diff[supp[2]]
+                
+                push!(yes,edge)
+                
+            end
+            
+        end
+        
+    end
+
+     return length(E) == length(yes)
+    
+end
+#this is way too slow and clunky, but it works
